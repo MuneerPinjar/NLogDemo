2011405718f7c00d1a5f60f3e7dec32819d3fb53
Muneer Pinjar <Muneer.Pinjar.ext@unit4.com>
2022-12-09 19:40:08 +0530

    added NLog config nuget package

--- NLogDemo/NLogDemo/Program.cs
+++ NLogDemo/NLogDemo/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using NLog;
+var logger =LogManager.GetCurrentClassLogger();
+
 Console.WriteLine("Hello, World!");
+logger.Error("This is Error Message");
+
 Console.ReadLine();
